6d862df705b378db5f6fe3b2286c3607d3ffa6e2
Lilith <lilith.takiryan@gmail.com>
2026-06-18 13:47:17 +0400

    Initial Commit

---
+++ fviu/fviuApp.swift
@@ -0,0 +1,17 @@
+//
+//  fviuApp.swift
+//  fviu
+//
+//  Created by lilit on 18.06.26.
+//
+
+import SwiftUI
+
+@main
+struct fviuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
